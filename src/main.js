6dbbb8c00d1cca03a9bac7944b216694a3f99e1b
Ryhus <ryhusyevhen@gmail.com>
2026-03-08 17:28:59 +0100

    feat: add CLI interface

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+import { cwd, stdin, stdout, env, chdir } from "node:process";
+import readline from "node:readline/promises";
+
+console.log("Welcome to Data Processing CLI!");
+chdir(env.HOME || env.USERPROFILE);
+console.log("You are currently in:", cwd());
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+rl.prompt();
+
+rl.on("line", (line) => {
+  if (line.trim() === ".exit") {
+    rl.close();
+    return;
+  }
+  console.log("You are currently in:", cwd());
+  rl.prompt();
+});
+
+rl.on("close", () => {
+  console.log("Thank you for using Data Processing CLI!");
+  process.exit(0);
+});
+
+rl.on("SIGINT", () => {
+  rl.close();
+});
