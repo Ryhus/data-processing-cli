849bf1fc6492c95c63912bd07518ff41c71a45ef
Ryhus <ryhusyevhen@gmail.com>
2026-03-12 17:10:32 +0100

    feat: add count feature

---
+++ src/commands/count.js
@@ -0,0 +1,51 @@
+import { parseArgs } from "node:util";
+import { createReadStream } from "node:fs";
+import path from "node:path";
+
+function countChar(args) {
+  let inputPath;
+  let buffer = "";
+  let lines = 0;
+  let words = 0;
+  let chars = 0;
+
+  try {
+    const { values } = parseArgs({
+      args,
+      options: { input: { type: "string" } },
+    });
+
+    inputPath = path.resolve(values.input);
+  } catch (error) {
+    console.log("Invalid input");
+    return;
+  }
+
+  const stream = createReadStream(inputPath);
+
+  stream.on("data", (chunk) => {
+    buffer += chunk.toString();
+    const splitedLines = buffer.split("\n");
+    buffer = splitedLines.pop();
+
+    for (const line of splitedLines) {
+      lines += 1;
+      chars += line.length + 1; // this +1 for the \n symbol
+      words += line.split(/\s+/).filter(Boolean).length;
+    }
+  });
+
+  stream.on("end", () => {
+    if (buffer.length > 0) {
+      lines += 1;
+      chars += buffer.length;
+      words += buffer.split(/\s+/).filter(Boolean).length;
+    }
+
+    console.log("Lines:", lines);
+    console.log("Words:", words);
+    console.log("Characters:", chars);
+  });
+}
+
+export { countChar };
--- src/repl.js
+++ src/repl.js
@@ -1,6 +1,7 @@
 import { upDir, changeDir, listFilesAndDirs } from "./navigation.js";
 import { csvToJson } from "./commands/csvToJson.js";
 import { jsonToCsv } from "./commands/jsonToCsv.js";
+import { countChar } from "./commands/count.js";
 
 const commands = {
   up: upDir,
@@ -8,6 +9,7 @@ const commands = {
   ls: listFilesAndDirs,
   "csv-to-json": csvToJson,
   "json-to-csv": jsonToCsv,
+  count: countChar,
 };
 
 async function commandParser(cmd) {
